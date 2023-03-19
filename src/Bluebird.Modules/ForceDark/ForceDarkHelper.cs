f02d990f863e50eb1c46a7eecc841949e2632726
JULIΛN (Pinguin2001) <75797743+Pinguin2001@users.noreply.github.com>
2023-03-19 16:22:26 +0100

    feat: Add ForceDark module

---
+++ src/Bluebird.Modules/ForceDark/ForceDarkHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Bluebird.Modules.ForceDark;
+
+public class ForceDarkHelper
+{
+    public static async Task<string> GetForceDarkScriptAsync()
+    {
+        StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Bluebird.Modules/ForceDark/Assets/forcedark.js"));
+        string jscript = await FileIO.ReadTextAsync(file);
+        return jscript;
+    }
+}
