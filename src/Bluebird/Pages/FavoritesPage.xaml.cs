7199eeff021dc7af19091df5356c346833975e84
JULIΛN (Pinguin2001) <75797743+Pinguin2001@users.noreply.github.com>
2023-04-08 15:42:57 +0200

    feat: Favorites context menu + clear all btn

--- src/Bluebird/Pages/FavoritesPage.xaml.cs
+++ src/Bluebird/Pages/FavoritesPage.xaml.cs
@@ -1,42 +1,90 @@
-﻿namespace Bluebird.Pages
+﻿namespace Bluebird.Pages;
+
+public sealed partial class FavoritesPage : Page
 {
-    public sealed partial class FavoritesPage : Page
+    public FavoritesPage()
     {
-        public FavoritesPage()
-        {
-            this.InitializeComponent();
-            LoadFavorites();
-        }
+        this.InitializeComponent();
+        LoadFavorites();
+    }
 
-        private async void LoadFavorites()
-        {
-            JsonItemsList = await Json.GetListFromJsonAsync("Favorites.json");
-            if (JsonItemsList != null)
-                FavoritesListView.ItemsSource = JsonItemsList;
-            else
-                FavoritesListView.ItemsSource = null;
-        }
+    private async void LoadFavorites()
+    {
+        JsonItemsList = await Json.GetListFromJsonAsync("Favorites.json");
+        if (JsonItemsList != null)
+            FavoritesListView.ItemsSource = JsonItemsList;
+        else
+            FavoritesListView.ItemsSource = null;
+    }
 
-        private void FavoritesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    private void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        TextBox textbox = sender as TextBox;
+        // Get all ListView items with the submitted search query
+        var SearchResults = from s in JsonItemsList where s.Title.Contains(textbox.Text, StringComparison.OrdinalIgnoreCase) select s;
+        // Set SearchResults as ItemSource for HistoryListView
+        FavoritesListView.ItemsSource = SearchResults;
+    }
+
+    private async void ClearAllButton_Click(object sender, RoutedEventArgs e)
+    {
+        ContentDialogResult result = await UI.ShowDialogWithAction("Delete all favorites?", "Do you want to clear all favorites?", "Clear", "Cancel");
+
+        if (result == ContentDialogResult.Primary)
+            await FileHelper.DeleteLocalFile("Favorites.json");
+        FavoritesListView.ItemsSource = null;
+    }
+
+    private void FavoritesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // Get listview sender
+        ListView listView = sender as ListView;
+        if (listView.ItemsSource != null)
         {
-            // Get listview sender
-            ListView listView = sender as ListView;
-            if (listView.ItemsSource != null)
-            {
-                // Get selected item
-                JsonItems item = (JsonItems)listView.SelectedItem;
-                launchurl = item.Url;
+            // Get selected item
+            JsonItems item = (JsonItems)listView.SelectedItem;
+            launchurl = item.Url;
+            MainPageContent.CreateWebTab();
+        }
+    }
+
+    string ctmtext;
+    string ctmurl;
+    private void FavoritesListView_RightTapped(object sender, RightTappedRoutedEventArgs e)
+    {
+        ListView listView = sender as ListView;
+        var options = new FlyoutShowOptions()
+        {
+            Position = e.GetPosition(listView),
+        };
+        FavoritesContextMenu.ShowAt(listView, options);
+        var item = ((FrameworkElement)e.OriginalSource).DataContext as JsonItems;
+        ctmtext = item.Title;
+        ctmurl = item.Url;
+    }
+
+    private async void FavContextItem_Click(object sender, RoutedEventArgs e)
+    {
+        switch ((sender as AppBarButton).Tag)
+        {
+            case "OpenLnkInNewWindow":
+                await Launcher.LaunchUriAsync(new Uri($"bluebird:{ctmurl}"));
+                break;
+            case "OpenLnkInNewTab":
+                launchurl = ctmurl;
                 MainPageContent.CreateWebTab();
-            }
+                break;
+            case "Copy":
+                SystemHelper.WriteStringToClipboard(ctmurl);
+                break;
+            case "CopyText":
+                SystemHelper.WriteStringToClipboard(ctmtext);
+                break;
+            // link context menu
+            case "ShareLink":
+                SystemHelper.ShowShareUIURL(ctmtext, ctmurl);
+                break;
         }
-
-        private void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            TextBox textbox = sender as TextBox;
-            // Get all ListView items with the submitted search query
-            var SearchResults = from s in JsonItemsList where s.Title.Contains(textbox.Text, StringComparison.OrdinalIgnoreCase) select s;
-            // Set SearchResults as ItemSource for HistoryListView
-            FavoritesListView.ItemsSource = SearchResults;
-        }
+        FavoritesContextMenu.Hide();
     }
 }
