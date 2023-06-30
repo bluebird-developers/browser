4f4ad671d5cc4e8007101e0b1469642e31c01802
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2023-06-30 18:12:49 +0200

    FRE: Add FREWrapper + FREPage1

---
+++ src/Bluebird/FRE/FREWrapper.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FREPages = Bluebird.FRE.Pages;
+
+namespace Bluebird.FRE;
+
+/// <summary>
+/// The wrapper for all FRE pages
+/// </summary>
+public sealed partial class FREWrapper : Page
+{
+    public FREWrapper()
+    {
+        this.InitializeComponent();
+        FREWrapperFrame.Navigate(typeof(FREPages.FREPage1));
+    }
+}
---
+++ src/Bluebird/FRE/Pages/FREPage1.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Bluebird.FRE.Pages;
+
+/// <summary>
+/// The first FRE page
+/// It welcomes the user to their newly downloaded browser
+/// </summary>
+public sealed partial class FREPage1 : Page
+{
+    public FREPage1()
+    {
+        this.InitializeComponent();
+    }
+
+    private void GetStartedButton_Click(object sender, RoutedEventArgs e)
+    {
+        //Frame.Navigate(typeof(FREPage2));
+    }
+}
