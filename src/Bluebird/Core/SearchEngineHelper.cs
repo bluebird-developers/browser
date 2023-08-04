e47bbbda63405330061cd3a91613d2e940473821
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2023-08-04 18:09:16 +0200

    refactor: Search engine selection

---
+++ src/Bluebird/Core/SearchEngineHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluebird.Core;
+
+public class SearchEngineHelper
+{
+    public static void SetSearchEngine(string selection)
+    {
+        if (selection == "Ask") SetEngine("Ask", "https://www.ask.com/web?q=");
+        if (selection == "Baidu") SetEngine("Baidu", "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=");
+        if (selection == "Bing") SetEngine("Bing", "https://www.bing.com?q=");
+        if (selection == "DuckDuckGo") SetEngine("DuckDuckGo", "https://www.duckduckgo.com?q=");
+        if (selection == "Ecosia") SetEngine("Ecosia", "https://www.ecosia.org/search?q=");
+        if (selection == "Google") SetEngine("Google", "https://www.google.com/search?q=");
+        if (selection == "Startpage") SetEngine("Startpage", "https://www.startpage.com/search?q=");
+        if (selection == "Qwant") SetEngine("Qwant", "https://www.qwant.com/?q=");
+        if (selection == "Qwant Lite") SetEngine("Qwant Lite", "https://lite.qwant.com/?q=");
+        if (selection == "Yahoo!") SetEngine("Yahoo!", "https://search.yahoo.com/search?p=");
+    }
+
+    private static void SetEngine(string EngineFriendlyName, string SearchUrl)
+    {
+        SettingsHelper.SetSetting("EngineFriendlyName", EngineFriendlyName);
+        SettingsHelper.SetSetting("SearchUrl", SearchUrl);
+        Globals.SearchUrl = SearchUrl;
+    }
+
+    public static List<string> SearchEngines = new()
+    {
+            "Ask",
+            "Baidu",
+            "Bing",
+            "DuckDuckGo",
+            "Ecosia",
+            "Google",
+            "Startpage",
+            "Qwant",
+            "Qwant Lite",
+            "Yahoo!"
+    };
+}
--- src/Bluebird/Pages/SettingPages/General.xaml.cs
+++ src/Bluebird/Pages/SettingPages/General.xaml.cs
@@ -12,6 +12,7 @@ public sealed partial class General : Page
 
     private void GetSettings()
     {
+        SearchEngineSelector.ItemsSource = SearchEngineHelper.SearchEngines;
         string SearchEngine = SettingsHelper.GetSetting("EngineFriendlyName");
         if (SearchEngine != null)
             SearchEngineSelector.PlaceholderText = SearchEngine;
@@ -20,7 +21,7 @@ public sealed partial class General : Page
 
         if (SettingsHelper.GetSetting("ForceDark") == "true")
             ForceDarkSwitch.IsOn = true;
-
+        
         // Set event handlers
         SearchEngineSelector.SelectionChanged += SearchEngineSelector_SelectionChanged;
         ForceDarkSwitch.Toggled += ForceDarkSwitch_Toggled;
@@ -36,23 +37,7 @@ public sealed partial class General : Page
     private void SearchEngineSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         string selection = e.AddedItems[0].ToString();
-        if (selection == "Ask") SetEngine("Ask", "https://www.ask.com/web?q=");
-        if (selection == "Baidu") SetEngine("Baidu", "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=");
-        if (selection == "Bing") SetEngine("Bing", "https://www.bing.com?q=");
-        if (selection == "DuckDuckGo") SetEngine("DuckDuckGo", "https://www.duckduckgo.com?q=");
-        if (selection == "Ecosia") SetEngine("Ecosia", "https://www.ecosia.org/search?q=");
-        if (selection == "Google") SetEngine("Google", "https://www.google.com/search?q=");
-        if (selection == "Startpage") SetEngine("Startpage", "https://www.startpage.com/search?q=");
-        if (selection == "Qwant") SetEngine("Qwant", "https://www.qwant.com/?q=");
-        if (selection == "Qwant Lite") SetEngine("Qwant Lite", "https://lite.qwant.com/?q=");
-        if (selection == "Yahoo!") SetEngine("Yahoo!", "https://search.yahoo.com/search?p=");
-    }
-
-    private void SetEngine(string EngineFriendlyName, string SearchUrl)
-    {
-        SettingsHelper.SetSetting("EngineFriendlyName", EngineFriendlyName);
-        SettingsHelper.SetSetting("SearchUrl", SearchUrl);
-        Globals.SearchUrl = SearchUrl;
+        SearchEngineHelper.SetSearchEngine(selection);
     }
 
     private void ForceDarkSwitch_Toggled(object sender, RoutedEventArgs e)
