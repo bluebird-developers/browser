e4084f40c26e31a3a90a0be4d824f97ff0d5ea13
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2024-09-25 12:31:37 +0200

    feat: Match single char url like x.com

--- src/Bluebird/Core/UrlHelper.cs
+++ src/Bluebird/Core/UrlHelper.cs
@@ -4,7 +4,7 @@ namespace Bluebird.Core;
 
 public class UrlHelper
 {
-    private static readonly Regex UrlMatch = new("^(http(s)?:\\/\\/.)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$", RegexOptions.Singleline);
+    private static readonly Regex UrlMatch = new("^(http(s)?:\\/\\/.)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$", RegexOptions.Singleline);
     private static readonly Regex IPMatch = new("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?::([0-9]{1,5}))?$", RegexOptions.Singleline);
     public static string GetInputType(string input)
     {
