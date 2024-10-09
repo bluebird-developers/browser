4aca1c4bc50c7f1350d6fa5de4a4be77fb49ab92
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2024-10-09 19:44:17 +0200

    refactor: Remove Microsoft.Toolkit.Uwp.Notifications dependency and use ToastNotifcation WinRT apis directly

--- src/Bluebird/Core/NotificationHelper.cs
+++ src/Bluebird/Core/NotificationHelper.cs
@@ -1,19 +1,18 @@
-﻿using Microsoft.Toolkit.Uwp.Notifications;
+﻿namespace Bluebird.Core;
 
-namespace Bluebird.Core
+internal class NotificationHelper
 {
-    internal class NotificationHelper
+    private static XmlDocument xmlDoc;
+    public static void NotifyUser(string header, string content)
     {
-        public static void NotifyUser(string header, string content)
+        string xmlString = $"<?xml version=\"1.0\" encoding=\"utf-8\"?><toast><visual><binding template=\"ToastGeneric\"><text>{header}</text><text>{content}</text></binding></visual></toast>";
+        xmlDoc ??= new XmlDocument();
+        xmlDoc.LoadXml(xmlString);
+        ToastNotification notification = new(xmlDoc)
         {
-            var builder = new ToastContentBuilder()
-                .AddText(header)
-                .AddText(content);
-            
-            builder.Show(toast =>
-            {
-                 toast.ExpirationTime = DateTime.Now.AddMinutes(5);
-            });
-        }
+            ExpirationTime = DateTime.Now.AddMinutes(5),
+            ExpiresOnReboot = true
+        };
+        ToastNotificationManager.CreateToastNotifier().Show(notification);
     }
 }
