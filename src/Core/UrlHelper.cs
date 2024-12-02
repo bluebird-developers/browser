d2b9fde30e020b1ff7be3e029852df54ce1d7784
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2024-12-02 18:13:18 +0100

    refactor: Url detection

--- src/Core/UrlHelper.cs
+++ src/Core/UrlHelper.cs
@@ -2,7 +2,7 @@
 
 public static class UrlHelper
 {
-    private static readonly Regex UrlMatch = new("^(http(s)?:\\/\\/.)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$", RegexOptions.Singleline | RegexOptions.Compiled);
+    private static readonly Regex UrlMatch = new(@"^(http(s)?:\/\/)?(www.)?([a-zA-Z0-9])+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,63}(:[0-9]{1,5})?(\/[^\s]*)?$", RegexOptions.Singleline | RegexOptions.Compiled);
     private static readonly Regex IPMatch = new("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?::([0-9]{1,5}))?$", RegexOptions.Singleline | RegexOptions.Compiled);
     public static string GetInputType(string input)
     {
