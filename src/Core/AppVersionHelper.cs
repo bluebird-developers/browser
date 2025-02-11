ab53fd577014f5da5d7a3d70083e3e65c0e38049
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2025-02-11 19:05:14 +0100

    refactor: Remove unused code

--- src/Core/AppVersionHelper.cs
+++ src/Core/AppVersionHelper.cs
@@ -10,13 +10,4 @@ public class AppVersionHelper
 
         return string.Format("{0}.{1}.{2}", version.Major, version.Minor, version.Build);
     }
-
-    public static string GetAppVersionCode()
-    {
-        Package package = Package.Current;
-        PackageId packageId = package.Id;
-        PackageVersion version = packageId.Version;
-
-        return string.Format("{0}{1}{2}", version.Major, version.Minor, version.Build);
-    }
 }
